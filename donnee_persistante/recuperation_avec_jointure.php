9695286988642f63ffef0d0c946799f17cd78a31
yanmonga <yannick.monga@ejsarl.com>
2023-09-30 18:35:36 +0100

    fix donnee persistante: recuperation avec jointure

---
+++ donnee_persistante/recuperation_avec_jointure.php
@@ -0,0 +1,66 @@
+<?php
+
+   $application_id=$json_decode->application_id;
+
+   $entite_id=$json_decode->entite_id; 
+
+   $nom=$json_decode->nom; 
+
+   $types=$json_decode->types; 
+
+   $taille=$json_decode->taille; 
+
+   $defaut=$json_decode->defaut;
+
+   $valeur=$json_decode->valeur;
+
+   $indexe=$json_decode->indexe;
+
+   $cle_primaire=$json_decode->cle_primaire;
+
+   $descriptions=$json_decode->descriptions;
+
+
+   try
+      {
+         $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
+
+         $stmt = $dbh->prepare("SELECT donnee_persistante.id, donnee_persistante.application_id, donnee_persistante.entite_id,donnee_persistante.nom, applications.id, entite.id FROM `donnee_persistante` INNER JOIN applications ON donnee_persistante.application_id=applications.id INNER JOIN entite ON donnee_persistante.entite_id=entite.id  WHERE donnee_persistante.application_id= ? AND donnee_persistante.entite_id= ? ");
+
+         $stmt->bindParam(1, $application_id);
+                     
+         $stmt->bindParam(2, $entite_id);
+
+         $stmt->execute();
+
+         $datas = array();
+
+         $nombreLigne = $stmt->rowCount();
+              
+         if($nombreLigne > 0)
+            { 
+               while($resultat=$stmt->fetch(PDO::FETCH_ASSOC)) 
+                  {
+                     $datas["code"]  = 200;
+
+                     $datas['donnee_persistante'][]=$resultat;
+                  }
+            }
+         else
+            {
+               $datas["code"]  = 400;
+      
+               $datas['token'][]="Ressource not found";
+            }
+               
+         echo json_encode($datas);
+      }
+
+   catch (PDOException $e)
+      {
+         print "Erreur !: " . $e->getMessage() . "<br/>";
+              
+         die();
+      }
+   
+   ?>
