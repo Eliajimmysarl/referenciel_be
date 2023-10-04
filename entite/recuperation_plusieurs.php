23375afd165ab5285027e8631e249824c9be4ea9
yanmonga <yannick.monga@ejsarl.com>
2023-10-04 11:33:35 +0100

    update entite recuperation plusieur

--- entite/recuperation_plusieurs.php
+++ entite/recuperation_plusieurs.php
@@ -1,36 +1,36 @@
 <?php
 
     try {
-            $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
+        $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
 
-            $stmt = $dbh->prepare("SELECT *FROM entite  ORDER BY id");
+        $stmt = $dbh->prepare("SELECT *FROM entite  ORDER BY id");
 
-            $stmt->execute();
+        $stmt->execute();
 
-            $datas = array();
+        $datas = array();
 
-            $nombreLigne = $stmt->rowCount();
-            
-            if($nombreLigne > 0)
-                { 
-                    while($resultat=$stmt->fetch(PDO::FETCH_ASSOC)) 
-                        {
-                            $datas["code"]  = 200;
-                            
-                            $datas['entite'][]=$resultat;
-                        }
-                }
-            else
-                {
-                    $datas["code"]  = 400;
+        $nombreLigne = $stmt->rowCount();
         
-                    $datas['token'][]="Ressource not found";
-                }   
-            echo json_encode($datas);
-            
-        }
+        if($nombreLigne > 0)
+            { 
+                while($resultat=$stmt->fetch(PDO::FETCH_ASSOC)) 
+                    {
+                        $datas["code"]  = 200;
+                        
+                        $datas['entite'][]=$resultat;
+                    }
+            }
+        else
+            {
+                $datas["code"]  = 400;
+
+                $datas['token'][]="Ressource not found";
+            }   
+        echo json_encode($datas);
+        
+    }
     catch (PDOException $e) {
-                print "Erreur !: " . $e->getMessage() . "<br/>";
-                die();
+            print "Erreur !: " . $e->getMessage() . "<br/>";
+            die();
     }
 ?> 
