d4df332dc540ff94390668d78cd14fcf9b02e514
yanmonga <yannick.monga@ejsarl.com>
2023-10-10 15:28:51 +0100

    entite :recuperation

--- entite/recuperation_avec_jointure.php
+++ entite/recuperation_avec_jointure.php
@@ -12,7 +12,7 @@
 
          $stmt = $dbh->prepare(" SELECT entite.id, entite.nom AS entite_nom, entite.application_id, applications.nom AS applications_nom FROM `entite` INNER JOIN applications ON entite.application_id=applications.id WHERE entite.application_id=? ");
 
-         $stmt->bindParam(1, $application_id);
+         $stmt->bindParam(1, $applicationId);
 
          $stmt->execute();
 
