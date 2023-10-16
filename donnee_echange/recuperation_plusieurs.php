9b78a010997f934a13eea0dbcbb6ddd9f6ca646f
yanmonga <yannick.monga@ejsarl.com>
2023-10-16 10:57:39 +0100

    fix recuperer plusieurs donnee composant

--- donnee_echange/recuperation_plusieurs.php
+++ donnee_echange/recuperation_plusieurs.php
@@ -3,7 +3,7 @@
     try {
             $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
 
-            $stmt = $dbh->prepare("SELECT donnee_echange.id, donnee_echange.application_id, donnee_echange.entite_id, donnee_echange.composant_id, donnee_echange.nom, donnee_echange.types, donnee_echange.descriptions, applications.id, entite.id, composant.id FROM `donnee_echange` INNER JOIN applications ON donnee_echange.application_id=applications.id INNER JOIN entite ON donnee_echange.entite_id=entite.id INNER JOIN composant ON donnee_echange.composant_id=composant.id ");
+            $stmt = $dbh->prepare("SELECT donnee_echange.id, donnee_echange.application_id, donnee_echange.entite_id, donnee_echange.composant_id, donnee_echange.nom, donnee_echange.types, donnee_echange.descriptions, applications.id, applications.nom AS application_nom, entite.id, entite.nom AS entite_nom, composant.id, composant.nom AS composant_nom FROM `donnee_echange` INNER JOIN applications ON donnee_echange.application_id=applications.id INNER JOIN entite ON donnee_echange.entite_id=entite.id INNER JOIN composant ON donnee_echange.composant_id=composant.id ");
 
             $stmt->execute();
 
