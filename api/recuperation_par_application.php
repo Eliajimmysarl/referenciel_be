e5d1926005dd616090169f89cb4200594fec7da2
yanmonga <yannick.monga@ejsarl.com>
2023-10-20 10:03:43 +0100

    update api recuperer avec jointure

--- api/recuperation_par_application.php
+++ api/recuperation_par_application.php
@@ -16,7 +16,7 @@
          $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
 
          
-         $stmt = $dbh->prepare("SELECT api.id, api.application_id, api.entite_id, api.composant_id, api.methode, api.uri, applications.id, applications.nom AS application_nom, entite.id, entite.nom AS entite_nom, composant.id, composant.nom AS composant_nom FROM `api` INNER JOIN applications ON api.application_id=applications.id INNER JOIN entite ON api.entite_id=entite.id INNER JOIN composant ON api.composant_id=composant.id WHERE api.application_id= ? ");
+         $stmt = $dbh->prepare("SELECT api.id, api.application_id, api.entite_id, api.composant_id, api.methode, api.uri, applications.nom AS application_nom, entite.nom AS entite_nom, composant.nom AS composant_nom FROM `api` INNER JOIN applications ON api.application_id=applications.id INNER JOIN entite ON api.entite_id=entite.id INNER JOIN composant ON api.composant_id=composant.id WHERE api.application_id= ? ");
 
          $stmt->bindParam(1, $applicationId);
 
