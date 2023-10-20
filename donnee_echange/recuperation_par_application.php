16028afb1cc55bee55e1b00dfdb93a2ae20c6033
yanmonga <yannick.monga@ejsarl.com>
2023-10-20 09:43:14 +0100

    update recuperer plusieurs donnee echange

--- donnee_echange/recuperation_par_application.php
+++ donnee_echange/recuperation_par_application.php
@@ -17,7 +17,7 @@
       {
          $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
 
-         $stmt = $dbh->prepare("SELECT donnee_echange.id, donnee_echange.application_id, donnee_echange.entite_id, donnee_echange.composant_id, donnee_echange.nom, donnee_echange.types, donnee_echange.descriptions, applications.id, applications.nom AS application_nom, entite.id, entite.nom AS entite_nom, composant.id, composant.nom AS composant_nom FROM `donnee_echange` INNER JOIN applications ON donnee_echange.application_id=applications.id INNER JOIN entite ON donnee_echange.entite_id=entite.id INNER JOIN composant ON donnee_echange.composant_id=composant.id  WHERE donnee_echange.application_id= ? ");
+         $stmt = $dbh->prepare("SELECT donnee_echange.id, donnee_echange.application_id, donnee_echange.entite_id, donnee_echange.composant_id, donnee_echange.nom, donnee_echange.types, donnee_echange.descriptions, applications.nom AS application_nom, entite.id, entite.nom AS entite_nom, composant.id, composant.nom AS composant_nom FROM `donnee_echange` INNER JOIN applications ON donnee_echange.application_id=applications.id INNER JOIN entite ON donnee_echange.entite_id=entite.id INNER JOIN composant ON donnee_echange.composant_id=composant.id  WHERE donnee_echange.application_id= ? ");
         
          $stmt->bindParam(1, $applicationId);
                      
