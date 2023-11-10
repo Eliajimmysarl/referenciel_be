89b6741d090fe3d666d1cb4da9bc6744fd308265
yanmonga <yannick.monga@ejsarl.com>
2023-11-10 15:27:11 +0100

    fix processus ajout, modifier et supprimer

--- processus/ajout_un_seul.php
+++ processus/ajout_un_seul.php
@@ -22,7 +22,7 @@
     try {
             $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
 
-            $stmt = $dbh->prepare("INSERT INTO entite (application_id, nom, descriptions, date_creation, date_update, heure_creation, heure_update) VALUES (?,?,?,?,?,?,?)");
+            $stmt = $dbh->prepare("INSERT INTO processus (application_id, nom, descriptions, date_creation, date_update, heure_creation, heure_update) VALUES (?,?,?,?,?,?,?)");
 
             $stmt->bindParam(1, $applicationId);
 
@@ -37,8 +37,6 @@
             $stmt->bindParam(6, $heureCreation);
 
             $stmt->bindParam(7, $heureUpdate);
-
-              
 
             $stmt->execute();
 
--- processus/suppression_un.php
+++ processus/suppression_un.php
@@ -4,13 +4,13 @@
     
             $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
 
-            $stmt = $dbh->prepare("DELETE FROM entite WHERE id = :id");
+            $stmt = $dbh->prepare("DELETE FROM processus WHERE id = :id");
 
             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
 
             $stmt->execute();
 
-            $stmt = $dbh->prepare("SELECT *FROM entite  ORDER BY id");
+            $stmt = $dbh->prepare("SELECT *FROM processus  ORDER BY id");
                     
             $stmt->execute();
                     
@@ -20,7 +20,7 @@
                         {
                             $datas["code"]  = 200;
 
-                            $datas['entite'][]=$resultat;
+                            $datas['processus'][]=$resultat;
                         }
                                 
             echo json_encode( $datas);
