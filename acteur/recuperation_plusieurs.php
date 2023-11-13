144cd2b497177c5d37cd755be17e683b159d9e1b
yanmonga <yannick.monga@ejsarl.com>
2023-11-13 15:22:34 +0100

    replace recuperer acteur

--- acteur/recuperation_plusieurs.php
+++ acteur/recuperation_plusieurs.php
@@ -3,8 +3,8 @@
     try {
         $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
 
-        $stmt = $dbh->prepare("SELECT acteur.id, acteur.nom, acteur.types, acteur.descriptions, applications.nom AS applications_nom, unite_organisation.nom AS unite_organisation_nom FROM `acteur` INNER JOIN applications ON acteur.application_id=applications.id INNER JOIN unite_organisation ON acteur.unite_organisation_id=unite_organisation.id ");
-
+        //$stmt = $dbh->prepare("SELECT acteur.id, acteur.nom, acteur.types, acteur.descriptions, applications.nom AS applications_nom, unite_organisation.nom AS unite_organisation_nom FROM `acteur` INNER JOIN applications ON acteur.application_id=applications.id INNER JOIN unite_organisation ON acteur.unite_organisation_id=unite_organisation.id ");
+        $stmt = $dbh->prepare("SELECT *FROM acteur   ORDER BY id");
         $stmt->execute();
 
         $datas = array();
