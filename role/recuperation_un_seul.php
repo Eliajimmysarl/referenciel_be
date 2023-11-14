cdee4a60b1a6e8849cb0e3e91dfb7af93d41e502
yanmonga <yannick.monga@ejsarl.com>
2023-11-14 11:39:28 +0100

    update recuperer un seul roles test

--- role/recuperation_un_seul.php
+++ role/recuperation_un_seul.php
@@ -4,7 +4,7 @@
         {
             $dbh = new PDO('mysql:host=localhost;dbname='.$db_referentiel, $user, $pass);
             
-            $stmt = $dbh->prepare("SELECT roles.id AS id_roles, roles.application_id, roles.acteur_id, roles.application_id, roles.unite_organisation_id, roles.nom, roles.descriptions, applications.nom AS application_nom, unite_organisation.id, unite_organisation.nom AS unite_organisation_nom, acteur.id, acteur.nom AS acteur_nom FROM `roles` INNER JOIN applications ON roles.application_id=applications.id INNER JOIN acteur ON roles.acteur_id=acteur.id INNER JOIN unite_organisation ON roles.unite_organisation_id=unite_organisation.id WHERE entite.id = :id");
+            $stmt = $dbh->prepare("SELECT roles.id AS id_roles, roles.application_id, roles.acteur_id, roles.application_id, roles.unite_organisation_id, roles.nom, roles.descriptions, applications.nom AS application_nom, unite_organisation.id, unite_organisation.nom AS unite_organisation_nom, acteur.id, acteur.nom AS acteur_nom FROM `roles` INNER JOIN applications ON roles.application_id=applications.id INNER JOIN acteur ON roles.acteur_id=acteur.id INNER JOIN unite_organisation ON roles.unite_organisation_id=unite_organisation.id WHERE roles.id=:id");
 
             $stmt->bindParam(':id', $id, PDO::PARAM_INT);
             
